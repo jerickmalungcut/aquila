891a46f51d553c98bcbbfeae31ef0b90cdf045dd
jerickmalungcut <jerickmalungcut@yahoo.com>
2025-11-18 23:27:07 +0800

    11/18

--- functions.php
+++ functions.php
@@ -7,5 +7,6 @@
 function aquila_enqueue_scripts() {
     wp_enqueue_style( 'aquila-style', get_stylesheet_uri()); // Enqueue main stylesheet
     wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true); // Enqueue main JavaScript file
+
 }
 add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' ); // Hook into WordPress
