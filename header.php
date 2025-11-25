8bbf43daae8ee82cfaa482ac82dc3bb0afdbe9d7
jerickmalungcut <jerickmalungcut@yahoo.com>
2025-11-25 23:47:34 +0800

    5th commit

--- header.php
+++ header.php
@@ -16,3 +16,14 @@
 <body <?php body_class(); ?>> <!-- Body tag with dynamic classes -->
     <?php wp_body_open(); ?> <!-- Hook for plugins to insert code right after the body tag -->
 
+    <header class="site-header">
+        <div class="container mx-auto px-4">
+            <h1 class="site-title text-3xl font-bold">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <?php bloginfo( 'name' ); ?> <!-- Dynamic site name -->
+                </a>
+            </h1>
+            <p class="site-description text-sm text-gray-600"></p>
+                <?php bloginfo( 'description' ); ?> <!-- Dynamic site description -->
+        </div>
+    </header>
